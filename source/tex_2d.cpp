3db3cabd5698ffc6974c92759afb3a10a1bb4cbe
Lily <gofastlily@gmail.com>
2024-04-24 20:51:14 -0400

    Simple Tex2d class

---
+++ source/tex_2d.cpp
@@ -0,0 +1,41 @@
+#include "tex_2d.hpp"
+
+
+namespace game {
+
+
+Tex2d::Tex2d() { }
+
+
+Tex2d::Tex2d(SDL_Texture *_texture, float _x_position, float _y_position, float _x_scale, float _y_scale) : texture(nullptr) {
+	texture = _texture;
+	x_position = _x_position;
+	y_position = _y_position;
+	x_scale = _x_scale;
+	y_scale = _y_scale;
+}
+
+
+void Tex2d::Render(SDL_Renderer *renderer) {
+    texture_rect = TextureRect(texture, x_position, y_position, x_scale, y_scale);
+	SDL_RenderCopy(renderer, texture, NULL, &texture_rect);
+}
+
+
+SDL_Rect Tex2d::TextureRect(SDL_Texture* texture, float x_position, float y_position, float x_scale, float y_scale) {
+	// Query the height and width of the texture
+	int texture_width;
+	int texture_height;
+	SDL_QueryTexture(texture, NULL, NULL, &texture_width, &texture_height);
+
+	// Build the required rect using the height and width
+	SDL_Rect rect;
+	rect.x = x_position;
+	rect.y = y_position;
+	rect.w = texture_width * x_scale;
+	rect.h = texture_height * y_scale;
+	return rect;
+}
+
+
+}	// namespace game
---
+++ source/tex_2d.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+
+// Linked library includes
+#include <SDL2/SDL.h>
+
+
+namespace game {
+
+
+class Tex2d {
+	public:
+		Tex2d();
+		Tex2d(SDL_Texture*, float, float, float = 1.0f, float = 1.0f);
+		SDL_Texture* texture;
+		SDL_Rect texture_rect;
+		float x_position;
+		float y_position;
+		float x_scale;
+		float y_scale;
+		void Render(SDL_Renderer*);
+		static SDL_Rect TextureRect(SDL_Texture*, float, float, float = 1.0f, float = 1.0f);
+};
+
+
+}	// namespace game
