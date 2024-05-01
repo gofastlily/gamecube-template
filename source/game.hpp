954f0dbe3db9fca958ecf2a99fb20fc55ad5fd2c
Lily <gofastlily@gmail.com>
2024-05-01 01:34:33 -0400

    Sample ponglike

--- source/game.hpp
+++ source/game.hpp
@@ -6,6 +6,9 @@
 
 
 // Local source includes
+#include "engine/text.hpp"
+#include "game/ball.hpp"
+#include "game/paddle.hpp"
 #include "input/input.hpp"
 
 
@@ -18,6 +21,17 @@ class Game {
 		void Init(SDL_Renderer*);
 		void Update(float, input::Input);
 		void Render(SDL_Renderer*);
+		void AIMovePaddleLeft();
+		void AIMovePaddleRight();
+		float AIMovePaddleTowardsBall(game::Paddle);
+		float AIMovePaddleTowardsCenter(game::Paddle);
+		game::Paddle paddle_left;
+		game::Paddle paddle_right;
+		game::Ball ball;
+		engine::Text score_left;
+		engine::Text score_right;
+		input::Gamepad ai_left_gamepad;
+		input::Gamepad ai_right_gamepad;
 };
 
 
