43a3a9a36b7872f3e2a6de24f7f44d2ff471b7a4
Lily <gofastlily@gmail.com>
2024-04-30 23:44:36 -0400

    Singleton for generating unique integer IDs

---
+++ source/engine/id.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+
+// Standard library includes
+#include <memory>
+
+
+namespace engine {
+
+
+class Id {
+	private:
+		Id() {}
+	public:
+		static Id& Instance() {
+			static Id instance;
+			return instance;
+		}
+		int NextUniqueId() {
+			return next_id++;
+		}
+		int next_id = 1;
+};
+
+
+}	// namespace utils
