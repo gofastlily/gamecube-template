6c8226102750bc8a7eae923e2397cbb6f056fdcc
Lily <gofastlily@gmail.com>
2024-05-01 19:29:30 -0400

    game object rotation fixes

--- source/engine/entities/game_object.cpp
+++ source/engine/entities/game_object.cpp
@@ -14,9 +14,11 @@ GameObject::GameObject(char* _name) {
 
 
 void GameObject::Update(float delta_time) {
-	if (transform.rotation <= -360.0f) transform.rotation += 360.0f;
-	if (transform.rotation >= 360.0f) transform.rotation -= 360.0f;
+	if (transform.rotation <= -180.0f) transform.rotation += 360.0f;
+	if (transform.rotation >= 180.0f) transform.rotation -= 360.0f;
 	if (velocity.x > maximum_velocity) velocity.x = maximum_velocity;
+	if (velocity.y <= -180.0f) velocity.y += 360.0f;
+	if (velocity.y >= 180.0f) velocity.y -= 360.0f;
 	transform.position.x += sin(velocity.y * M_PI / 180.0f) * velocity.x * delta_time;
 	transform.position.y += cos(velocity.y * M_PI / 180.0f) * velocity.x * delta_time;
 }
